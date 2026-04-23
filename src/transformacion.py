74ff658f6de2e8b6c52b0c7d11edfa2b0f8fd42d
esteban95-a <dpachuri@cesde.net>
2026-04-23 16:31:37 -0500

    feature : creo data frame df_entrega_de_calificaciones

--- src/transformacion.py
+++ src/transformacion.py
@@ -7,3 +7,6 @@ print(df_notas.columns.tolist())
 
 df_informacion_estudiante = df_notas[['id_nota', 'id_estudiante', 'id_materia', 'periodo', 'fecha_registro', 'observacion']]
 print(df_informacion_estudiante.head())
+
+df_entrega_de_calificaciones = df_notas[['id_nota', 'nota1', 'nota2', 'nota3']]
+print(df_entrega_de_calificaciones.head())
