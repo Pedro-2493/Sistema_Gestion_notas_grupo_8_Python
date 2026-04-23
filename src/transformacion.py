9d13fc814ae8eaf5311269e584a0504481bfa889
esteban95-a <dpachuri@cesde.net>
2026-04-23 16:09:13 -0500

    feature : creo data frame df_informacion_estudiante

--- src/transformacion.py
+++ src/transformacion.py
@@ -5,3 +5,5 @@ print(df_notas.head())
 
 print(df_notas.columns.tolist())
 
+df_informacion_estudiante = df_notas[['id_nota', 'id_estudiante', 'id_materia', 'periodo', 'fecha_registro', 'observacion']]
+print(df_informacion_estudiante.head())
